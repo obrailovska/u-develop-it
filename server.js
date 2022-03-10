8d7912c6a035149b91a4c1a16fd1a674e3c3488e
obrailovska <olabrrai@icloud.com>
2022-03-10 17:43:21 -0500

    added delete and update routs

--- server.js
+++ server.js
@@ -138,6 +138,34 @@ app.get("/api/party/:id", (req, res) => {
     });
   });
 });
+// Update a candidate's party
+app.put("/api/candidate/:id", (req, res) => {
+  const errors = inputCheck(req.body, "party_id");
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Candidate not found",
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
 // delete party
 app.delete("/api/party/:id", (req, res) => {
   const sql = `DELETE FROM parties WHERE id = ?`;
